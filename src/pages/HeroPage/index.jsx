b30fffafce246499222f62e5c500fb5c4e3f18a5
smadeira22 <saramariaramosmadeira@gmail.com>
2023-12-14 14:36:51 +0000

    fix api

--- src/pages/HeroPage/index.jsx
+++ src/pages/HeroPage/index.jsx
@@ -4,12 +4,15 @@ import axios from "axios"
 
 import { HeroCard } from '../../components'
 
+
+
 const HeroPage = () => {
   const [results, setResults] = useState({ image: {}})
   const { id } = useParams()
+
   useEffect(() => {
     const displayHero = async () => {
-      const { data } = await axios.get(`https://superheroapi.com/api/2859083194232848/1`)
+      const { data } = await axios.get(`https://superheroapi.com/api.php/2859083194232848/1`)
       console.log(data)
       setResults(data)
     }
